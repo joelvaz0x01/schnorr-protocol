63c03928dbd6609d3e4e76448f8429912c671b41
Joel Vaz <joelvaz.whitehat@gmail.com>
2024-04-01 20:42:17 +0100

    Code cleanup

--- src/main.c
+++ src/main.c
@@ -1,9 +1,29 @@
-#include <stdio.h>
 #include <openssl/bn.h>
+#include <stdio.h>
 
 #define PRIME_SECURITY 2048
 #define SECURITY_LEVEL 1024
 #define PROOF_ITERATIONS 1000
+
+/**
+ * Free a BIGNUM variable
+ * @param bn BIGNUM variable to be freed
+ * @return NULL
+ */
+void *free_bn(BIGNUM **bn) {
+    BN_clear_free(*bn);
+    return NULL;
+}
+
+/**
+ * Free a BN_CTX variable
+ * @param ctx BN_CTX variable to be freed
+ * @return NULL
+ */
+void *free_ctx(BN_CTX **ctx) {
+    BN_CTX_free(*ctx);
+    return NULL;
+}
 
 /**
  * Generates two safe prime numbers, p and q, where p = 2q + 1
@@ -14,27 +34,32 @@
 int generate_safe_primes(BIGNUM **p, BIGNUM **q) {
     BN_CTX *ctx = BN_CTX_secure_new();
 
+    if (!ctx) {
+        fprintf(stderr, "Error: Memory allocation failed\n");
+        return 1;
+    }
+
     if (!BN_generate_prime_ex2(*p, PRIME_SECURITY, 1, NULL, NULL, NULL, ctx)) {
         fprintf(stderr, "Failed to generate prime p.\n");
-        BN_CTX_free(ctx);
+        ctx = free_ctx(&ctx);
         return 1;
     }
 
     // Calculate p - 1
     if (!BN_sub(*q, *p, BN_value_one())) {
         fprintf(stderr, "Failed to subtract 1 from p.\n");
-        BN_CTX_free(ctx);
+        ctx = free_ctx(&ctx);
         return 1;
     }
 
     // Calculate q: (p - 1) / 2
     if (!BN_rshift1(*q, *q)) {
         fprintf(stderr, "Failed to calculate q.\n");
-        BN_CTX_free(ctx);
+        ctx = free_ctx(&ctx);
         return 1;
     }
 
-    BN_CTX_free(ctx);
+    ctx = free_ctx(&ctx);
     return 0;
 }
 
@@ -53,50 +78,52 @@ int generate_key_pair(const BIGNUM *p, const BIGNUM *q, BIGNUM **alpha, BIGNUM *
 
     if (!ctx) {
         fprintf(stderr, "Error: Memory allocation failed\n");
-        goto cleanup;
+        return 1;
     }
 
     if (!BN_priv_rand_range(*alpha, p)) {
         fprintf(stderr, "Error: Failed to generate alpha\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate: P - 1
     if (!BN_sub(*beta, p, BN_value_one())) {
         fprintf(stderr, "Error: Failed to calculate P - 1\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate: (P - 1) / Q
     if (!BN_div(*beta, NULL, *beta, q, ctx)) {
         fprintf(stderr, "Error: Failed to divide P - 1 by Q\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate: α^((P-1)/Q) mod P
     if (!BN_mod_exp(*beta, *alpha, *beta, p, ctx)) {
         fprintf(stderr, "Error: Failed to calculate beta\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Generate private key
     if (!BN_priv_rand_range(*a, q)) {
         fprintf(stderr, "Error: Failed to generate private key\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate public key (β^−a mod P)
     if (!BN_mod_exp(*v, BN_mod_inverse(NULL, *beta, p, ctx), *a, p, ctx)) {
         fprintf(stderr, "Error: Failed to generate public key\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
-    BN_CTX_free(ctx);
+    ctx = free_ctx(&ctx);
     return 0;
-
-    cleanup:
-    BN_CTX_free(ctx);
-    return 1;
 }
 
 /**
@@ -113,27 +140,25 @@ int generate_nonce(const BIGNUM *p, const BIGNUM *q, const BIGNUM *beta, BIGNUM 
 
     if (!ctx) {
         fprintf(stderr, "Error: Memory allocation failed\n");
-        goto cleanup;
+        return 1;
     }
 
     // Generate private nonce
     if (!BN_priv_rand_range(*r, q)) {
         fprintf(stderr, "Error: Failed to generate private nonce\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate public nonce
     if (!BN_mod_exp(*x, beta, *r, p, ctx)) {
         fprintf(stderr, "Error: Failed to calculate public nonce\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
-    BN_CTX_free(ctx);
+    ctx = free_ctx(&ctx);
     return 0;
-
-    cleanup:
-    BN_CTX_free(ctx);
-    return 1;
 }
 
 /**
@@ -150,37 +175,39 @@ int generate_challenge(BIGNUM **e) {
 
     if (!two || !sec_level || !ctx) {
         fprintf(stderr, "Error: Memory allocation failed\n");
-        goto cleanup;
+        return 1;
     }
 
     // Set variables: 2 and sec_level
     if (!BN_set_word(two, 2) || !BN_set_word(sec_level, (unsigned int) SECURITY_LEVEL)) {
         fprintf(stderr, "Error: Failed to set variables\n");
-        goto cleanup;
+        two = free_bn(&two);
+        sec_level = free_bn(&sec_level);
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate interval: 2^sec_level
     if (!BN_exp(sec_level, two, sec_level, ctx)) {
         fprintf(stderr, "Error: Failed to calculate interval\n");
-        goto cleanup;
+        two = free_bn(&two);
+        sec_level = free_bn(&sec_level);
+        ctx = free_ctx(&ctx);
+        return 1;
     }
-    BN_clear_free(two);
+    two = free_bn(&two);
 
     // Generate challenge
     if (!BN_priv_rand_range(*e, sec_level)) {
         fprintf(stderr, "Error: Failed to generate challenge\n");
-        BN_CTX_free(ctx);
+        sec_level = free_bn(&sec_level);
+        ctx = free_ctx(&ctx);
         return 1;
     }
 
-    BN_CTX_free(ctx);
+    sec_level = free_bn(&sec_level);
+    ctx = free_ctx(&ctx);
     return 0;
-
-    cleanup:
-    BN_clear_free(two);
-    BN_clear_free(sec_level);
-    BN_CTX_free(ctx);
-    return 1;
 }
 
 /**
@@ -196,27 +223,25 @@ int make_proof(const BIGNUM *q, const BIGNUM *a, const BIGNUM *e, const BIGNUM *
 
     if (!ctx) {
         fprintf(stderr, "Error: Memory allocation failed\n");
-        goto cleanup;
+        return 1;
     }
 
     // Calculate a * e
     if (!BN_mul(*y, a, e, ctx)) {
         fprintf(stderr, "Error: Failed to multiply a and e\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate y: (a * e + r) mod q
     if (!BN_mod_add(*y, *y, r, q, ctx)) {
         fprintf(stderr, "Error: Failed to sum a*e and r\n");
-        goto cleanup;
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
-    BN_CTX_free(ctx);
+    ctx = free_ctx(&ctx);
     return 0;
-
-    cleanup:
-    BN_CTX_free(ctx);
-    return 1;
 }
 
 /**
@@ -228,42 +253,43 @@ int make_proof(const BIGNUM *q, const BIGNUM *a, const BIGNUM *e, const BIGNUM *
  * @param z Bob's verification of Alice's identity
  * @return 0 if success, 1 if error
  */
-int
-calculate_proof(const BIGNUM *beta, const BIGNUM *y, const BIGNUM *v, const BIGNUM *e, const BIGNUM *p, BIGNUM **z) {
+int calculate_proof(const BIGNUM *beta, const BIGNUM *y, const BIGNUM *v, const BIGNUM *e, const BIGNUM *p,
+                    BIGNUM **z) {
     BIGNUM *z_aux = BN_secure_new();
     BN_CTX *ctx = BN_CTX_secure_new();
 
     if (!z_aux || !ctx) {
         fprintf(stderr, "Error: Memory allocation failed\n");
-        goto cleanup;
+        return 1;
     }
 
     // Calculate β^y mod p
     if (!BN_mod_exp(z_aux, beta, y, p, ctx)) {
         fprintf(stderr, "Error: Failed to calculate β^y\n");
-        goto cleanup;
+        z_aux = free_bn(&z_aux);
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate v^e mod p
     if (!BN_mod_exp(*z, v, e, p, ctx)) {
         fprintf(stderr, "Error: Failed to calculate v^e\n");
-        goto cleanup;
+        z_aux = free_bn(&z_aux);
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
     // Calculate z: β^y * v^e mod p
     if (!BN_mod_mul(*z, z_aux, *z, p, ctx)) {
         fprintf(stderr, "Error: Failed to calculate β^y * v^e\n");
-        goto cleanup;
+        z_aux = free_bn(&z_aux);
+        ctx = free_ctx(&ctx);
+        return 1;
     }
 
-    BN_clear_free(z_aux);
-    BN_CTX_free(ctx);
+    z_aux = free_bn(&z_aux);
+    ctx = free_ctx(&ctx);
     return 0;
-
-    cleanup:
-    BN_clear_free(z_aux);
-    BN_CTX_free(ctx);
-    return 1;
 }
 
 
@@ -282,7 +308,7 @@ int main() {
 
     if (!p || !q || !alpha || !beta || !a || !v || !r || !x || !e || !y || !z) {
         fprintf(stderr, "Memory allocation failed.\n");
-        goto cleanup;
+        return -1;
     }
 
     // Generate safe primes
@@ -297,9 +323,8 @@ int main() {
         fprintf(stderr, "Error generating key pair.\n");
         goto cleanup;
     }
-    printf("Generator α: %s\nGenerator β: %s\n", BN_bn2dec(alpha), BN_bn2dec(beta));
+    printf("Generator Alpha: %s\nGenerator Beta: %s\n", BN_bn2dec(alpha), BN_bn2dec(beta));
     printf("Private key: %s\nPublic key: %s\n", BN_bn2dec(a), BN_bn2dec(v));
-
 
     int nTimes = 0, right = 0, wrong = 0;
 
@@ -336,34 +361,34 @@ int main() {
         nTimes++;
     }
 
-    printf("Final report:\n");
+    printf("\nFinal report:\n");
     printf(" - True: %d/%d - %d%%\n", right, nTimes, right / nTimes * 100);
     printf(" - False: %d/%d - %d%%\n", wrong, nTimes, wrong / nTimes * 100);
 
-    BN_clear_free(p);
-    BN_clear_free(q);
-    BN_clear_free(alpha);
-    BN_clear_free(beta);
-    BN_clear_free(a);
-    BN_clear_free(v);
-    BN_clear_free(r);
-    BN_clear_free(x);
-    BN_clear_free(e);
-    BN_clear_free(y);
-    BN_clear_free(z);
+    p = free_bn(&p);
+    q = free_bn(&q);
+    alpha = free_bn(&alpha);
+    beta = free_bn(&beta);
+    a = free_bn(&a);
+    v = free_bn(&v);
+    r = free_bn(&r);
+    x = free_bn(&x);
+    e = free_bn(&e);
+    y = free_bn(&y);
+    z = free_bn(&z);
     return 0;
 
-    cleanup:
-    BN_clear_free(p);
-    BN_clear_free(q);
-    BN_clear_free(alpha);
-    BN_clear_free(beta);
-    BN_clear_free(a);
-    BN_clear_free(v);
-    BN_clear_free(r);
-    BN_clear_free(x);
-    BN_clear_free(e);
-    BN_clear_free(y);
-    BN_clear_free(z);
+cleanup:
+    p = free_bn(&p);
+    q = free_bn(&q);
+    alpha = free_bn(&alpha);
+    beta = free_bn(&beta);
+    a = free_bn(&a);
+    v = free_bn(&v);
+    r = free_bn(&r);
+    x = free_bn(&x);
+    e = free_bn(&e);
+    y = free_bn(&y);
+    z = free_bn(&z);
     return -1;
 }
